eee4c88c31e0aca0a2589628efe20ae34f70bb5a
kaomeyly <kaomeyly@gmail.com>
2026-03-22 20:20:35 +0700

    first commit

---
+++ test.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>//header file
+#include<conio.h>
+
+int main(){
+	
+//	printf("Hello Guy\n");
+//	printf("Hello Teacher\3");
+    printf("\t****Pratice****");
+    printf("\n \t1.CPP is Mid-Level Language.");
+    printf("\n \t2.CPP is create by Dennis Ritchie.");
+    printf("\n \t3.CPP is create in 1972.");
+    printf("\n \t=====Thank you!\4====");
+
+
+    
+	
+	getch(); 
+	return 0;
+}
